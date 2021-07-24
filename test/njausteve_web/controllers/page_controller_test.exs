4dda70611d8c7de2b85a58bfaccb35448779dda0
stephen <njaustevedomino@gmail.com>
2021-07-24 22:42:29 +0800

    add header (#17)
    * refactor: extract out gtm scripts into partials
    
    Signed-off-by: Stephen <njaustevedomino@gmail.com>
    
    * feat: change header and add in normalize and fonts
    
    Signed-off-by: Stephen <njaustevedomino@gmail.com>
    
    * fix: Remove boilerplace markup
    
    Signed-off-by: Stephen <njaustevedomino@gmail.com>
    
    * test: update page test
    
    Signed-off-by: Stephen <njaustevedomino@gmail.com>

--- test/njausteve_web/controllers/page_controller_test.exs
+++ test/njausteve_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule NjausteveWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Njausteve"
   end
 end
