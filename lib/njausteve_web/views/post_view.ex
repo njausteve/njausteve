8a9cff6d767b032987a8de1b2f418b79fc221d6e
Stephen <njaustevedomino@gmail.com>
2021-10-09 00:36:15 +0800

    feat: :lipstick: add the posts ui + side navigation
    Signed-off-by: Stephen <njaustevedomino@gmail.com>

--- lib/njausteve_web/views/post_view.ex
+++ lib/njausteve_web/views/post_view.ex
@@ -1,3 +1,28 @@
 defmodule NjausteveWeb.PostView do
   use NjausteveWeb, :view
+
+  def truncate(text, opts \\ []) do
+    max_length = opts[:max_length] || 50
+    omission = opts[:omission] || "..."
+
+    cond do
+      not String.valid?(text) ->
+        text
+
+      String.length(text) < max_length ->
+        text
+
+      true ->
+        length_with_omission = max_length - String.length(omission)
+
+        "#{String.slice(text, 0, length_with_omission)}#{omission}"
+    end
+  end
+
+  def path_list(path) do
+    case path_list = Path.split(path) do
+      ["/" | tail] -> tail
+      _ -> path_list
+    end
+  end
 end
