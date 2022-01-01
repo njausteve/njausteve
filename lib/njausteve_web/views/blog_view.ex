812ae778fab25f4ac3bfe8358802bfec07781345
njausteve <njaustevedomino@gmail.com>
2022-01-01 19:08:31 +0800

    feat(njausteveWeb): update blog page and individual blog
    Signed-off-by: njausteve <njaustevedomino@gmail.com>

--- lib/njausteve_web/views/blog_view.ex
+++ lib/njausteve_web/views/blog_view.ex
@@ -1,3 +1,17 @@
 defmodule NjausteveWeb.BlogView do
   use NjausteveWeb, :view
+
+  alias Njausteve.Posts.Post
+
+  def timeToRead(%Post{body: body}) when byte_size(body) > 0 do
+    body
+    |> String.split(~r{(\\n|[^\w'])+})
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.count()
+    |> div(200)
+  end
+
+  def timeToRead(_) do
+    0
+  end
 end
