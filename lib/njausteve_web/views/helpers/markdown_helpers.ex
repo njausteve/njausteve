631608c94732985c70562e8ac8a5dbb9824e55b4
njausteve <njaustevedomino@gmail.com>
2022-02-06 15:38:01 +0800

    feat: add function to extract image src - add floki
    Signed-off-by: njausteve <njaustevedomino@gmail.com>

--- lib/njausteve_web/views/helpers/markdown_helpers.ex
+++ lib/njausteve_web/views/helpers/markdown_helpers.ex
@@ -4,12 +4,34 @@ defmodule NjausteveWeb.MarkdownHelpers do
   """
   use Phoenix.HTML
 
+  @default_image "https://picsum.photos/1600/900"
+
   @doc """
   Converts markdown to html.
   """
   def markdown(body) do
     body
-    |> Earmark.as_html!()
+    |> Earmark.as_html!(pure_links: false)
     |> raw
+  end
+
+  def get_image_src(html_body) do
+    html_body
+    |> Earmark.as_html!()
+    |> Floki.parse_document()
+    |> case do
+      {:ok, document} ->
+        document
+        |> Floki.find("img")
+        |> Enum.flat_map(&Floki.attribute(&1, "src"))
+        |> Enum.at(0)
+        |> case do
+          src when is_binary(src) -> src
+          _ -> @default_image
+        end
+
+      _ ->
+        @default_image
+    end
   end
 end
